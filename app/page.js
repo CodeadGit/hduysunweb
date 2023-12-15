d6d936120c692cc4db96b976714c378f67326d85
FarukKeskinsoy <133097328+codeaddev@users.noreply.github.com>
2023-12-15 19:12:24 +0300

    scripted...

--- app/page.js
+++ app/page.js
@@ -24,7 +24,7 @@ export default function Home() {
       />
     <div className={`wrapper ${mode}`}>
       <BikHeader/>
-      <FacebookPixel/>
+      {/* <FacebookPixel/> */}
       {!storyModall ? (
         <HomePage />
       ) : (
