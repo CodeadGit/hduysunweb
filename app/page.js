f0a0bbfef1f46fae2eeb304e7da5c8d57e149581
FarukKeskinsoy <133097328+codeaddev@users.noreply.github.com>
2023-12-18 01:51:50 +0300

    gtag

--- app/page.js
+++ app/page.js
@@ -8,23 +8,46 @@ import BikHeader from "@/components/BikHeader";
 import FacebookPixel from "@/components/FacebookPixel";
 import Head from "next/head";
 import Script from "next/script";
+import * as gtag from '../lib/gtag'
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function Home() {
+
   const { mode } = useModeContext();
   const { storyModall } = useAdsContext();
   const modeStatus = mode === "dark";
-
   return (
     <>
+    <Head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'UA-205477047-35', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
+      </Head>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=UA-205477047-35`}
+      />
       <Script
         src="https://cdn.p.analitik.bik.gov.tr/tracker.js"
         strategy="lazyOnload"
         data-website-id="657f41c4-4e9d-405a-95e8-84eb9ec7719a"
         data-host-url="//657f41c4-4e9d-405a-95e8-84eb9ec7719a.collector.p.analitik.bik.gov.tr"
       />
+      
     <div className={`wrapper ${mode}`}>
       <BikHeader/>
-      {/* <FacebookPixel/> */}
+      <FacebookPixel/>
       {!storyModall ? (
         <HomePage />
       ) : (
